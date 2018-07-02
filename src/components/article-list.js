6324d65d5638a2dd5ef0925700f929cb9c89d3f6
Roman Iakobchuk <romania@wix.com>
2018-07-02 22:35:24 +0300

    add decorator

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -1,21 +1,8 @@
 import React, { Component } from 'react'
 import Article from './article'
+import accordion from '../decorators/accordion'
 
 class ArticleList extends Component {
-  /*
-    constructor(...args) {
-        super(...args)
-
-        this.state = {
-            openArticleId: null
-        }
-    }
-*/
-
-  state = {
-    openArticleId: null
-  }
-
   render() {
     return <ul>{this.articles}</ul>
   }
@@ -25,14 +12,12 @@ class ArticleList extends Component {
       <li key={article.id}>
         <Article
           article={article}
-          isOpen={this.state.openArticleId === article.id}
-          toggleOpen={this.toggleOpenArticle}
+          isOpen={this.props.openItemId === article.id}
+          toggleOpen={this.props.toggleOpenItem}
         />
       </li>
     ))
   }
-
-  toggleOpenArticle = (openArticleId) => this.setState({ openArticleId })
 }
 
-export default ArticleList
+export default accordion(ArticleList)
---
+++ src/decorators/accordion.js
@@ -0,0 +1,21 @@
+//HOC === Higher Order Component === decorator
+import React from 'react'
+
+export default (OriginalComponent) =>
+  class DecoratedComponent extends React.Component {
+    state = {
+      openItemId: null
+    }
+
+    toggleOpenItem = (openItemId) => this.setState({ openItemId })
+
+    render() {
+      return (
+        <OriginalComponent
+          {...this.props}
+          {...this.state}
+          toggleOpenItem={this.toggleOpenItem}
+        />
+      )
+    }
+  }

1e8b45c8191df2fc4ee4e5960ae848de6b4ec71b
Alex Khatsianovich <micropasa@gmail.com>
2018-07-04 22:03:18 +0300

    Задача "## HT1.3 Реализовать multi-select"

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import ArticlesChart from './components/chart'
 
 class App extends Component {
   state = {
-    selected: null
+    selected: []
   }
 
   handleSelectChange = (selected) => this.setState({ selected })
@@ -23,6 +23,7 @@ class App extends Component {
           options={this.options}
           onChange={this.handleSelectChange}
           value={this.state.selected}
+          isMulti
         />
         <ArticleList articles={articles} ref={this.setListRef} />
         <ArticlesChart articles={articles} />

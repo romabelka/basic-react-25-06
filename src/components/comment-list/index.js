aee924ca13ef3b5ff2d8afe1a0edb5a6decb9c23
Roman Konkin <k.ramzes.r@gmail.com>
2018-07-08 14:09:19 +0300

    HT2.1 - add comment-list animation

--- src/components/comment-list/index.js
+++ src/components/comment-list/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import Comment from './comment'
-import toggleOpen from '../decorators/toggleOpen'
+import Comment from '../comment'
+import toggleOpen from '../../decorators/toggleOpen'
+import CSSTransition from 'react-addons-css-transition-group'
+import './style.css'
 
 class CommentList extends Component {
   static propTypes = {
@@ -23,7 +25,13 @@ class CommentList extends Component {
     return (
       <div>
         <button onClick={toggleOpen}>{text}</button>
-        {this.getBody()}
+        <CSSTransition
+          transitionName="comment-list"
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={150}
+        >
+          {this.getBody()}
+        </CSSTransition>
       </div>
     )
   }

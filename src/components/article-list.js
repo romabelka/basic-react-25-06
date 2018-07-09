00b26966455bae31d6b8eb2cef7f930264d0548b
Roman Iakobchuk <romania@wix.com>
2018-07-09 14:40:38 +0300

    add HW2

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import Article from './article'
 import accordion from '../decorators/accordion'
 
 export class ArticleList extends Component {
+  static propTypes = {
+    articles: PropTypes.array.isRequired,
+    fetchData: PropTypes.func,
+
+    //from accordion decorator
+    openItemId: PropTypes.string,
+    toggleItem: PropTypes.func
+  }
+
   componentDidMount() {
     this.props.fetchData && this.props.fetchData()
   }

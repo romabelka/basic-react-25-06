2eb0c3c8b7147c0640eaaef4054741e2ee05c8c8
pmishchenko <spider007gold@mail.ru>
2018-07-13 10:01:24 +0300

    HT 4.3

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,7 +1,20 @@
 import { DELETE_ARTICLE } from '../constants'
 import { normalizedArticles } from '../fixtures'
+import { OrderedMap, Record } from 'immutable'
 
-export default (articlesState = normalizedArticles, action) => {
+const Article = Record({
+  id: '',
+  date: '',
+  title: '',
+  text: '',
+  comments: []
+})
+
+const defaultArticles = normalizedArticles.reduce((acc, article) => {
+  return acc.set(article.id, new Article(article))
+}, new OrderedMap({}))
+
+export default (articlesState = defaultArticles, action) => {
   const { type, payload } = action
 
   switch (type) {
